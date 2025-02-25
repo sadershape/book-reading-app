f3bb29a94a5bcd4a80b331b9d01a11fa87323cee
sadershape <231564@astanait.edu.kz>
2025-02-25 23:44:04 +0500

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    quizResults: [
+      {
+        quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
+        score: Number,
+        completedAt: Date,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving user
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare entered password with stored hash
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
