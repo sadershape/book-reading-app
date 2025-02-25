b2cf30c469da1e2d086591b71666c46372b9b41a
sadershape <231564@astanait.edu.kz>
2025-02-25 23:59:35 +0500

    Create generateToken.js

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+export default generateToken;
